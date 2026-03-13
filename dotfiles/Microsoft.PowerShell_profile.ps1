ded2d2e87f5f0dccfba76bd2d26d4084e4530abf
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2026-03-13 14:45:49 +0300

    Add PowerShell profile with custom functions and aliases

---
+++ dotfiles/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,57 @@
+New-Alias -Name grep -Value Select-String
+
+function l { Get-ChildItem }
+
+function d { Set-Location ~\Desktop }
+
+function conf { code $PROFILE }
+
+function gitl { git log --oneline --graph --decorate }
+
+function clearHistory { Remove-Item (Get-PSReadlineOption).HistorySavePath }
+
+function ports { netstat -aof }
+
+function pp { Set-Location $env:PROJECTS_HOME }
+
+function p([string]$project_name = "") {
+    if ($project_name -ne "") { return Set-Location $env:PROJECTS_HOME/$project_name }
+
+    $folders = Get-ChildItem -Path $env:PROJECTS_HOME -Directory | Select-Object -ExpandProperty Name;
+
+    $running = $true
+    $selected = 0
+    $rowCount = $folders.Count + 2
+    $clearPreviousOutput = "$([char]27)[$($rowCount)A$([char]27)[J"
+
+    while ($running) {
+        Write-Host "============= Select the project =============" -ForegroundColor Yellow
+        for ($i = 0; $i -lt $folders.Count; $i++) {
+            if ($i -eq $selected) {
+                Write-Host "• $($folders[$i])" -ForegroundColor Green
+            } else {
+                Write-Host "  $($folders[$i])"
+            }
+        }
+        Write-Host "== [p]: projects directory - [q|Esc]: Quit ==" -ForegroundColor Yellow
+
+        $input = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        switch ($input.VirtualKeyCode) {
+            {$_ -in 40,74} { # j or down
+                $selected = ($selected + 1) % $folders.Count
+                Write-Host -NoNewLine $clearPreviousOutput
+            } 
+            {$_ -in 38,75} { # k or up
+                $selected = ($selected - 1 + $folders.Count) % $folders.Count 
+                Write-Host -NoNewLine $clearPreviousOutput
+            }
+            {$_ -in 27,81} { $running = $false } # q or esc
+            80 { return Set-Location $env:PROJECTS_HOME } # p
+            13 { # enter
+                $selected_project = $folders[$selected]
+                $running = $false 
+                return Set-Location -Path "$env:PROJECTS_HOME/$selected_project"
+            }
+        }
+    }
+}
